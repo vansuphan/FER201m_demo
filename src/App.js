1429c69eb120249737881d4a500b7834b283e7d6
vansuphan <vansuphan.kma@gmail.com>
2023-09-21 16:16:55 +0700

    update day 4

--- src/App.js
+++ src/App.js
@@ -6,18 +6,45 @@ import Card from "./components/carrd/Card";
 function App() {
   return (
     <div className="App">
+      <section className='section-1 mt-5'>
+        <div className="container">
+          <div className='row'>
+            <div className='col-6 col-sm-6 col-md-6 col-lg-3'>
+                <h4>col 1</h4>
+            </div>
+            <div className='col-6 col-sm-6 col-md-6 col-lg-3'>
+                <h4>col 2</h4>
+            </div>
+            <div className='col-6 col-sm-6 col-md-6 col-lg-3'>
+                <h4>col 3</h4>
+            </div>
+            <div className='col-6 col-sm-6 col-md-6 col-lg-3'>
+                <h4>col 4</h4>
+            </div>
+            <div className='col-6 col-sm-6 col-md-6 col-lg-4'>
+                <h4>col 5</h4>
+            </div>
+            <div className='col-6 col-sm-6 col-md-6 col-lg-4'>
+                <h4>col 6</h4>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* <hr /> */}
+
+      {/* <Button value="FER201m" />
       <Button value="FER201m" />
       <Button value="FER201m" />
-      <Button value="FER201m" />
-      <Button value="FER201m" />
-      <hr />
-      <div className="list-card" style={{padding: "30px"}}>
+      <Button value="FER201m" /> */}
+      {/* <hr /> */}
+      {/* <div className="list-card" style={{padding: "30px"}}>
         <Card title="Test" />
         <Card />
         <Card />
         <Card />
         <Card />
-      </div>
+      </div> */}
     </div>
   );
 }
