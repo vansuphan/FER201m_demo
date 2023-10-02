a6feeaf13fc7d608364a1658ce59f2a9cd5309cf
vansuphan <vansuphan.kma@gmail.com>
2023-10-02 16:26:10 +0700

    update UI

--- src/pages/login/LoginPage.js
+++ src/pages/login/LoginPage.js
@@ -3,6 +3,10 @@ import styles from "./LoginPage.module.scss";
 // import FormLogin from '../../components/form-login/FormLogin';
 import FormLoginNormal from "../../components/form-login/FormLoginNormal";
 import SalyImg from "../../assets/images/Saly-1.png";
+import Ellipse3 from '../../assets/images/Ellipse3.png';
+import Ellipse4 from '../../assets/images/Ellipse4.png';
+import CancelIcon from '../../assets/icons/icon-cancel.png';
+
 
 export default function LoginPage() {
   return (
@@ -25,7 +29,24 @@ export default function LoginPage() {
           </div>
         </div>
       </section>
-      <section className={styles.Footer}></section>
+      <section className={styles.Footer}>
+        <div className={styles.container}>
+          <div className={styles.listItem}>
+            <div className={styles.item}>
+              <img src={Ellipse3} alt="" />
+              <h4>John peter</h4>
+              <p>Active 1 days ago</p>
+              <img className={styles.CancelIcon} src={CancelIcon} alt="" />
+            </div>
+            <div className={styles.item}>
+              <img src={Ellipse4} alt="" />
+              <h4>Alina shmen</h4>
+              <p>Active 4 days ago</p>
+              <img className={styles.CancelIcon} src={CancelIcon} alt="" />
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
