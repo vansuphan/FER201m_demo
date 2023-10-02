1e227d33b66b60367ffdf6159c98c1cb52750fb1
vansuphan <vansuphan.kma@gmail.com>
2023-10-02 15:51:38 +0700

    update UI

--- src/components/form-login/FormLoginNormal.js
+++ src/components/form-login/FormLoginNormal.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { useForm } from 'react-hook-form';
 import styles from './FormLogin.module.scss';
 import { Link } from 'react-router-dom';
+import GoogleIcon from '../../assets/icons/google.png';
+import FbIcon from '../../assets/icons/facebook.png';
+import AppleIcon from '../../assets/icons/apple.png';
 
 export default function FormLoginNormal() {
   const {
@@ -24,11 +27,24 @@ export default function FormLoginNormal() {
         </div>
       </div>
       <h3>Sign in</h3>
+      <div className={styles.groupButton}>
+        <button>
+          <img alt='' srcSet={GoogleIcon} />
+          <span>Sign in with Google</span>
+        </button>
+        <button>
+          <img alt='' srcSet={FbIcon} />
+        </button>
+        <button>
+          <img alt='' srcSet={AppleIcon} />
+        </button>
+      </div>
       <div className={styles.fromItem}>
         <label htmlFor='username'>Enter your username or email address</label>
         <input
           id='username' type="text" maxLength={255} 
           name='username'
+          placeholder='Username or email address'
         />
       </div>
       <div className={styles.fromItem}>
@@ -38,6 +54,7 @@ export default function FormLoginNormal() {
           name='password'
           type="text"
           maxLength={255} 
+          placeholder='Password'
         />
         <a href='#'>Forgot Password</a>
       </div>
